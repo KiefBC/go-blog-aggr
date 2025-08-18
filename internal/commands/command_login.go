b7f081669e5ba253d114cf11f0f9d6a8caac8d30
KiefBC <kiefhay642@outlook.com>
2025-08-17 23:03:41 -0700

    feat: ability to login

---
+++ internal/commands/command_login.go
@@ -0,0 +1,27 @@
+package commands
+
+import (
+	"context"
+	"fmt"
+	"os"
+)
+
+func HandlerLogin(s *State, cmd Command) error {
+	if len(cmd.Args) < 1 {
+		return fmt.Errorf("login command requires a username argument")
+	}
+
+	user, err := s.Db.GetUser(context.Background(), cmd.Args[0])
+	if err != nil {
+		fmt.Println("user not found:", err)
+		os.Exit(1)
+	}
+
+	err = s.Config.SetUser(user.Name)
+	if err != nil {
+		return fmt.Errorf("couldn't set current user: %w", err)
+	}
+
+	fmt.Printf("Setting current user to %s\n", s.Config.Current_user_name)
+	return nil
+}
