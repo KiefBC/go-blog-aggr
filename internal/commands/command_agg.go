98eabd4c5282bceaccaf4bb64b5057f948a2fb4e
KiefBC <kiefhay642@outlook.com>
2025-08-19 11:11:02 -0700

    feat: ability to poll our rss feeds with a given interval

--- internal/commands/command_agg.go
+++ internal/commands/command_agg.go
@@ -3,26 +3,55 @@ package commands
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/KiefBC/blog-aggr/internal/rss"
 )
 
 func HandlerAgg(s *State, cmd Command) error {
-	// TODO: Uncomment when ready to use command arguments
-
-	// if len(cmd.Args) < 1 {
-	// 	return fmt.Errorf("Usage: %s", s.Commands.GetUsage(cmd.Name))
-	// }
-	//
-	// feedURL := cmd.Args[0]
-
-	feedURL := "https://www.wagslane.dev/index.xml"
-	feed, err := rss.FetchFeed(context.Background(), feedURL)
-	if err != nil {
-		return fmt.Errorf("failed to fetch feed %s: %v", feedURL, err)
+	if len(cmd.Args) < 1 {
+		return fmt.Errorf("Usage: %s", s.Commands.GetUsage(cmd.Name))
 	}
 
-	fmt.Printf("%+v\n", feed)
+	time_between_req, err := time.ParseDuration(cmd.Args[0])
+	if err != nil {
+		return fmt.Errorf("invalid duration format: %v", err)
+	}
+	fmt.Printf("Collecting feeds every %s\n", time_between_req)
+
+	ticker := time.NewTicker(time_between_req)
+	for ; ; <-ticker.C {
+		err := scrapeFeeds(s)
+		if err != nil {
+			fmt.Printf("Error scraping feeds: %v\n", err)
+		}
+	}
+}
+
+func scrapeFeeds(s *State) error {
+	nextFeed, err := s.Db.GetNextFeedToFetch(
+		context.Background(),
+	)
+	if err != nil {
+		return fmt.Errorf("error getting next feed to fetch: %w", err)
+	}
+
+	_, err = s.Db.MarkFeedFetched(
+		context.Background(),
+		nextFeed.ID,
+	)
+	if err != nil {
+		return fmt.Errorf("error marking feed as fetched: %w", err)
+	}
+
+	feed, err := rss.FetchFeed(context.Background(), nextFeed.Url)
+	if err != nil {
+		return fmt.Errorf("error fetching feed %s: %w", nextFeed.Url, err)
+	}
+
+	for _, item := range feed.Channel.Item {
+		fmt.Printf("%+v\n", item.Title)
+	}
 
 	return nil
 }
